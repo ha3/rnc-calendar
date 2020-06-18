08dc5e9b8bdc7e096c606079355a560f99824ed3
Hakan Özdemir <hakan@artistanbul.io>
2020-06-18 17:16:54 +0300

    Fix Text import

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text as RNText } from 'react-native';
 
 
 const Text = ({ children,...props }) => {
   return (
-    <Text style={...props}>
+    <RNText style={...props}>
       {children}
-    </Text>
+    </RNText>
   );
 }
 
